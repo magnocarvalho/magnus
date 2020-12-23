b16e432b9741d18b605eb23c563b5fe9c184ec79
Magno Carvalho <magnocarv@hotmail.com>
2020-12-23 01:03:25 -0300

    feat: get stared angular 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/login/login.component';
+import { FormComponent } from './component/form/form.component';
+import { InputComponent } from './component/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FormComponent,
+    InputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
